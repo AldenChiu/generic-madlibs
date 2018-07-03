de80fe1dd24791676c9e7ef8044959f798a6ecf6
student <aldenchiu13@gmail.com>
2018-07-03 11:14:18 -0600

    Changed Title

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,7 @@ export default class App extends Component {
   render() {
     return (
       <div>
-        <h1>DevCamp React Starter</h1>
+        <h1>The megaphones are GALLOPING down the HILLS!</h1>
       </div>
     );
   }
