7aa7371efe0c575cf89b287c1784dcab72423956
student <aldenchiu13@gmail.com>
2018-07-05 11:32:48 -0600

    Added more styles!

--- src/components/header.js
+++ src/components/header.js
@@ -3,6 +3,8 @@ import React from 'react';
 const Header = () => {
     return (
         <div className="header">
+            <div className="header__skew"/>
+            <div className="header__border"/>
             <h1>{'Generic MadLibs'}</h1>
             <p>{'The megaphones are GALLOPING down the HILLS'}</p>
         </div>
